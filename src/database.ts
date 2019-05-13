2cbf875880abb6cf5975dc525e367cf5d7897c37
Raúl Negrón <raul.esteban.negron@gmail.com>
2019-05-12 20:10:40 -0400

    Update DB connection config

--- src/database.ts
+++ src/database.ts
@@ -3,10 +3,13 @@ import * as mongoose from 'mongoose';
 const host = process.env.DATABASE_HOST || 'localhost';
 const port = process.env.DATABASE_PORT || '27017';
 const database = process.env.DATABASE_NAME || 'cc-api';
-const dbUrl = `mongodb://${host}:${port}/${database}`;
+const dbUrl =
+  process.env.NODE_ENV === 'production'
+    ? process.env.PRODUCTION_DB_URI
+    : `mongodb://${host}:${port}/${database}`;
 
 export default () => {
-  return mongoose.connect(dbUrl, {
+  return mongoose.connect(dbUrl || `mongodb://${host}:${port}/${database}`, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
